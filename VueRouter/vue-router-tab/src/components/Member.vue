dff4b847de1256df288e55008c5cf2e60b1f8dff
k <developerlyk@gmail.com>
2024-03-31 18:01:15 +0900

    Vue-router for문으로 router 출력

---
+++ VueRouter/vue-router-tab/src/components/Member.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Member</h1>
+</template>
+
+<style scoped></style>
--- VueRouter/vue-router-tab/src/main.js
+++ VueRouter/vue-router-tab/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import routes from "./route";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
